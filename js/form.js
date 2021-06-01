7c3dd8f5d11c64a29f0dcc08162190416293f9ad
MyClubs171 <85178520+MyClubs171@users.noreply.github.com>
2021-06-01 18:13:50 +0200

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,52 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAEBAT2ZZXPXFxNtggCYBiOMpuJvj-jpsw",
+  authDomain: "myclubs-ba7ab.firebaseapp.com",
+  projectId: "myclubs-ba7ab",
+  storageBucket: "myclubs-ba7ab.appspot.com",
+  messagingSenderId: "987570133368",
+  appId: "1:987570133368:web:53bc6293a1c707d956108d",
+  measurementId: "G-98TK0JZGTS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  function signUp(){
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed Up");
+
+    window.location='home.html';
+  }
+
+  function signIn() {
+    
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed In");
+
+    // Take user to a different or home page
+
+    window.location='home.html';
+  }
+
+  function signOut() {
+
+    auth.signOut();
+    alert("Signed Out");
+    window.location='Login.html';
+  }
+  
